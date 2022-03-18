1f81830fc3b3eba5fe83b4672a20470e29272afd
Gabriel Rosas <TheRealTheriel@gmail.com>
2022-03-17 21:14:38 -0700

    added sword

--- player.py
+++ player.py
@@ -1,15 +1,16 @@
-import pygame
+from pygame.image import load as load_image
+from pygame.transform import rotate
 from pygame import draw
 from config import FPS
 from pygame.key import get_pressed as get_keys_pressed
 from pygame.constants import K_UP, K_DOWN, K_RIGHT, K_LEFT, K_w, K_a, K_s, K_d
 from enum import Enum
-
+import weapons
 import math
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
-# s
+
 
 class ControllerLayout(Enum):  # for different keyboard movements
     WASD = [K_w, K_a, K_s, K_d]
@@ -27,20 +28,24 @@ class Position:  # to handle x and y stuff more easily
 
 class Player:
     def __init__(self, layout: ControllerLayout):
+        self.weapon = weapons.Sword(10, load_image("resources/images/swords/broadsword.png"))
         self.input_keys = layout
-        self.color = BLUE if layout.name == "WASD" else RED
 
-        self.image = pygame.image.load("resources/images/broyalguard.bmp")
-        if self.color == RED:
-            self.image = pygame.image.load("resources/images/rroyalguard.bmp")
+        self.image = load_image("resources/images/broyalguard.bmp") if layout.name == "WASD" else load_image("resources/images/rroyalguard.bmp")  # python trolling
+
         self.position = Position(50, 50)
         self.velocity = 500
         self.rotation_velocity = 500
         self.angle = 90 # Measured in degrees
 
+
     def update(self, screen):
-        rotated_image = pygame.transform.rotate(self.image, self.angle-90)
-        screen.blit(rotated_image, (self.position.x, self.position.y))
+        rotated_image = rotate(self.image, self.angle-90)
+        self.draw_weapon(screen)
+        screen.blit(rotated_image, self.position.xy())
+
+    def draw_weapon(self, screen):
+        self.weapon.draw(screen, self.position, self.angle)
 
     def handle_movement(self):
         keys = get_keys_pressed()
---
+++ weapons.py
@@ -0,0 +1,18 @@
+from pygame.transform import rotate
+import math
+
+class Sword:
+    def __init__(self, damage: int, sprite):
+        self.damage = damage
+        self.image = sprite
+
+    def draw(self, screen, position, angle):
+        rotated_image = rotate(self.image, angle - 90)
+
+        to_radian = math.radians(angle)
+        dx = 30*math.cos(to_radian)
+        dy = 30*math.sin(to_radian)
+
+        screen.blit(rotated_image, (position.x + dx, position.y - dy))
+
+
